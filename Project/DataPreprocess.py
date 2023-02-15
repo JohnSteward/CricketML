c6bd8d44cebd3b77cc57a0987beca0b3216da9b1
John Steward <js3511@bath.ac.uk>
2023-02-15 22:44:58 +0000

    Just realised I can't use PCA as a lot of data is not numerical

--- Project/DataPreprocess.py
+++ Project/DataPreprocess.py
@@ -1,2 +1,9 @@
 import pandas as pd
-import sklearn
+import numpy as np
+from sklearn.decomposition import PCA
+
+ballData = pd.read_pickle("ballData.pkl")
+
+pca = PCA(n_components=50)
+pca.fit(ballData)
+
