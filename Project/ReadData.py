5b92c2f7312431329fdb56bdd2f6db4f4a5dc586
John Steward <js3511@bath.ac.uk>
2023-03-13 15:48:53 +0000

    Revert "Started working on feature vectors"
    This reverts commit ec13315ca862d7eb5a50f562e1dc15b41f02451f.

--- Project/ReadData.py
+++ Project/ReadData.py
@@ -18,25 +18,12 @@ import glob
 ballDataFrames = []
 pathToBall = 'C:/Users/John Steward/Documents/GitHub/BachelorProject/Project/ipl_2022_data/BallData/*'
 ballList = glob.glob(pathToBall)
-batsmanList = []
-bowlerList = []
+
 for file in ballList:
     # Load the json file into a dataframe and flatten the nested dictionaries
     with open(file, 'r') as f:
         data = json.loads(f.read())
     df = pd.json_normalize(data, max_level=3)
-    # Create a list of players to correspond to a feature vector for each (so far will be a separate dataframe)
-    batsmanName = df["match.battingTeam.batsman.name"]
-    if batsmanName not in batsmanList:
-        batsmanList.append(batsmanName)
-    bowlerName = df["match.bowlingTeam.bowler.name"]
-    if bowlerName not in bowlerList:
-        bowlerList.append(bowlerName)
-
-    # TODO: Create a list of dataframes, one for each player as a feature vector, corresponding to batsmanList and
-    #       bowlerList
-    #  Make all string data categorical (like shotPlayed, attacked...)
-
     # Drop consistent or unnecessary columns
     df.drop(['country', 'format', 'international', 'tourName', 'match.name'], axis=1, inplace=True)
     ballDataFrames.append(df)
