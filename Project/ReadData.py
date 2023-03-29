e948646c3df5d9b40d655808c2d9f24b06ecd86b
John Steward <js3511@bath.ac.uk>
2023-03-29 14:54:25 +0100

    Replaced all strings (I think)

--- Project/ReadData.py
+++ Project/ReadData.py
@@ -18,6 +18,12 @@ import glob
 ballDataFrames = []
 batsmanList = []
 bowlerList = []
+deliveryList = []
+extrasList = []
+weightList = []
+attackedList = []
+playedList = []
+addShotTypeList = []
 pathToBall = 'C:/Users/John Steward/Documents/GitHub/BachelorProject/Project/ipl_2022_data/BallData/*'
 ballList = glob.glob(pathToBall)
 
@@ -27,7 +33,10 @@ for file in ballList:
         data = json.loads(f.read())
     df = pd.json_normalize(data)
     # Drop consistent or unnecessary columns
-    df.drop(['country', 'format', 'international', 'tourName', 'match.name'], axis=1, inplace=True)
+    df.drop(['country', 'format', 'international', 'tourName', 'match.name', 'match.battingTeam.id',
+             'match.battingTeam.batsman.id', 'match.bowlingTeam.bowlerPartner.id',
+             'match.bowlingTeam.bowlerPartner.isRightHanded', 'match.bowlingTeam.bowlerPartner.name',
+             'match.delivery.timecode', 'match.delivery.shotInformation.batsmanWeight'], axis=1, inplace=True)
     ballDataFrames.append(df)
  # Create a list of players to correspond to a feature vector for each (so far will be a separate dataframe)
     batsmanName = df["match.battingTeam.batsman.name"]
@@ -36,24 +45,58 @@ for file in ballList:
     bowlerName = df["match.bowlingTeam.bowler.name"]
     if bowlerName.item() not in bowlerList:
         bowlerList.append(bowlerName.item())
+    deliveryType = df["match.delivery.deliveryType"]
+    if deliveryType.item() not in deliveryList:
+        deliveryList.append(deliveryType.item())
+    extras = df["match.delivery.scoringInformation.extrasType"]
+    if extras.item() not in extrasList:
+        extrasList.append(extras.item())
+    attacked = df["match.delivery.shotInformation.shotAttacked"]
+    if attacked.item() not in attackedList:
+        attackedList.append(attacked.item())
+    played = df["match.delivery.shotInformation.shotPlayed"]
+    if played.item() not in playedList:
+        playedList.append(played.item())
+    addShot = df["match.delivery.shotInformation.shotTypeAdditional"]
+    if addShot.item() not in addShotTypeList:
+        addShotTypeList.append(addShot.item())
     # TODO: Create a list of dataframes, one for each player as a feature vector, corresponding to batsmanList and
     #       bowlerList
-    #  Make all string data categorical (like shotPlayed, attacked...)
+    #  Make all string data categorical (like shotPlayed, attacked...). Compile a list of all the delivery types,
+    #  then run through the dataframe, replacing the strings with the index in the list.
+print(deliveryList)
+print(extrasList)
+print(attackedList)
+print(playedList)
+print(addShotTypeList)
 
-# need categorical data for this and delivery type
-attackedNo = 0
-defendNo = 0
+# Replacing all relevant strings with a numerical ID
+for file in ballDataFrames:
+    for i in range(len(deliveryList) - 1):
+        if deliveryList[i] == file["match.delivery.deliveryType"].item():
+            file["match.delivery.deliveryType"] = file["match.delivery.deliveryType"].replace(deliveryList[i], i)
+    for j in range(len(extrasList) - 1):
+        if extrasList[j] == file["match.delivery.scoringInformation.extrasType"].item():
+            file["match.delivery.scoringInformation.extrasType"] = file["match.delivery.scoringInformation.extrasType"].replace(extrasList[j], j)
+    for k in range(len(attackedList) - 1):
+        if attackedList[k] == file["match.delivery.shotInformation.shotPlayed"].item():
+            file["match.delivery.shotInformation.shotPlayed"] = file["match.delivery.shotInformation.shotPlayed"].replace(attackedList[k], k)
+    for l in range(len(playedList) - 1):
+        if playedList[l] == file["match.delivery.shotInformation.shotPlayed"].item():
+            file["match.delivery.shotInformation.shotPlayed"] = file["match.delivery.shotInformation.shotPlayed"].replace(playedList[l], l)
+    for m in range(len(addShotTypeList) - 1):
+        if addShotTypeList[m] == file["match.delivery.shotInformation.shotTypeAdditional"].item():
+            file["match.delivery.shotInformation.shotTypeAdditional"] = file["match.delivery.shotInformation.shotTypeAdditional"].replace(addShotTypeList[m], m)
+
 for batsman in batsmanList:
     totalRuns = 0
     totalBalls = 0
-    for bowler in bowlerList:
-        for file in ballDataFrames:
-            if (file["match.battingTeam.batsman.name"].item() == batsman) and (file["match.bowlingTeam.bowler.name"].item() == bowler):
-                totalRuns += file["match.delivery.scoringInformation.score"].item()
-                totalBalls += 1
-            elif file["match.battingTeam.batsman.name"].item() == batsman:
-                totalRuns += file["match.delivery.scoringInformation.score"].item()
-                totalBalls += 1
+    attackedNo = 0
+    defendNo = 0
+    for file in ballDataFrames:
+        if file["match.battingTeam.batsman.name"].item() == batsman:
+            totalRuns += file["match.delivery.scoringInformation.score"].item()
+            totalBalls += 1
 
 for bowler in bowlerList:
     totalBalls = 0
