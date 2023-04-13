bcfea099bb41b13341380c917256db56a2232db7
John Steward <js3511@bath.ac.uk>
2023-04-13 13:36:07 +0100

    plotted data

--- Project/GPRegression.py
+++ Project/GPRegression.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 import numpy as np
 import sklearn
 from sklearn.gaussian_process import GaussianProcessClassifier as gpc
@@ -8,9 +8,13 @@ from sklearn.model_selection import train_test_split
 ballData = pd.read_pickle("ballData.pkl")
 ballData.info()
 
+
+hist = ballData.plot.hist(column=['match.delivery.scoringInformation.score'], bins=7, range=[0, 7])
+plt.show()
 # Split data into train(80%) and test(20%)
-train, test = train_test_split(ballData, test_size=0.2, shuffle=True)
-print(train.shape, test.shape)
-target = train['match.delivery.scoringInformation.score']
-model = gpc()
-model.fit(train, target)
+# train, test = train_test_split(ballData, test_size=0.2, shuffle=True)
+# print(train.shape, test.shape)
+# # extract the feature that we want to predict
+# target = train['match.delivery.scoringInformation.score']
+# model = gpc()
+# model.fit(train, target)
