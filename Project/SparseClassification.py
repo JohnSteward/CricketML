b143b915ddc55da07e219aedeb35d333fa715b8d
John Steward <js3511@bath.ac.uk>
2023-04-14 21:32:32 +0100

    THERE ARE NO NP.FLOATS!!!!!

--- Project/SparseClassification.py
+++ Project/SparseClassification.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import sklearn
-from GPy.models import OneVsAllSparseClassification
-from sklearn.gaussian_process import GaussianProcessClassifier as gpc
+from GPy.models import OneVsAllSparseClassification, OneVsAllClassification
+
 from sklearn.model_selection import train_test_split
 
 ballData = pd.read_pickle("ballData.pkl")
@@ -18,9 +18,20 @@ train, test = train_test_split(ballData, test_size=0.2, shuffle=True)
 print(train.shape, test.shape)
 index = 0
 # extract the feature that we want to predict
-train.columns = train.columns.str.replace(' ', '')
-train['match.delivery.scoringInformation.score'] = train['match.delivery.scoringInformation.score'].astype('int32')
 train.info()
 target = train['match.delivery.scoringInformation.score'].values[np.newaxis]
-print(target.dtype)
-model = OneVsAllSparseClassification(train, target.T)
+print(type(target.T[0][0]))
+trainy = []
+for datapoint in train.values:
+    for i in range(56, 79):
+        datapoint[i] = float(datapoint[i])
+    trainy.append(datapoint)
+realTrain = np.asarray(trainy)
+print(type(realTrain[0][70]))
+for i in realTrain:
+    for j in range(56, 79):
+        if not (isinstance(i[j], float)):
+            print(j)
+print(realTrain)
+model = OneVsAllSparseClassification(realTrain, target.T)
+print("trained")
