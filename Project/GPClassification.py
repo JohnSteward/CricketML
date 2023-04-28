3ba1c28f4e62d0a50f103f989ee5060d9055fd09
John Steward <js3511@bath.ac.uk>
2023-04-28 15:59:09 +0100

    Added word docs

--- Project/GPClassification.py
+++ Project/GPClassification.py
@@ -12,11 +12,11 @@ ballData.info()
 # hist = ballData.plot.hist(column=['match.delivery.scoringInformation.score'], bins=8, range=[-1, 7])
 # plt.show()
 
-fileName = "model1.pkl"
+fileName = "model50%1.pkl"
 
 # Split data into train(70%) and test(30%)
-train, test = train_test_split(ballData, test_size=0.3, shuffle=True)
-# testy, exclusion = train_test_split(test, test_size=0.6, shuffle=True)
+train, test = train_test_split(ballData, test_size=0.5, shuffle=True)
+testy, exclusion = train_test_split(test, test_size=0.6, shuffle=True)
 
 #realTest, exclusion = train_test_split(test, test_size=0.8, shuffle=True)
 #print(train.shape, realTest.shape)
@@ -25,11 +25,11 @@ train, test = train_test_split(ballData, test_size=0.3, shuffle=True)
 # extract the feature that we want to predict
 train.info()
 target = train['match.delivery.scoringInformation.score'].values
-testTarget = test['match.delivery.scoringInformation.score'].values
+testTarget = testy['match.delivery.scoringInformation.score'].values
 #print(target.dtype)
 kernel = 1.0*RBF(1.0)
 model = gpc(kernel=kernel, n_jobs=-1).fit(train, target)
 print("trained")
 
 pickle.dump(model, open(fileName, "wb"))
-print(model.score(test, testTarget))
+print(model.score(testy, testTarget))
